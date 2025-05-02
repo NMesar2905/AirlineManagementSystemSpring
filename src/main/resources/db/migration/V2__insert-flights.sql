ab843870a2dcff74ea41a30c412de51793a9e3f0
Nicolas Mesa <nicolas.mesa@unosquare.com>
2025-05-02 10:27:46 -0500

    Updating SQL Script of Flights Creation

--- src/main/resources/db/migration/V2__insert-flights.sql
+++ src/main/resources/db/migration/V2__insert-flights.sql
@@ -6,6 +6,6 @@ INSERT INTO flight (f_code, f_name, source, destination) VALUES
 ('11223345', 'MEDMAN', 'Medellin', 'Manizales'),
 ('11223346', 'MANMED', 'Manizales', 'Medellin'),
 ('11223347', 'BOGMAN', 'Bogota', 'Manizales'),
-('11223348', 'MANBOG', 'Manizales', 'Bogota');
-('11223349', 'NEVMAN', 'Neiva', 'Manizales');
+('11223348', 'MANBOG', 'Manizales', 'Bogota'),
+('11223349', 'NEVMAN', 'Neiva', 'Manizales'),
 ('11223350', 'MANNEV', 'Manizales', 'Neiva');
